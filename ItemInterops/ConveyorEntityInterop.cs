80adad04ebe2d1b287f45a23d7e09aebd433c6ad
Nikey <Nikey646@users.noreply.github.com>
2016-06-10 09:47:12 +1000

    Removed .Net 4.5+ reference library, since compiling for .Net 3.5 Reduced the amount of double casts in ConveyorEntityInterop

--- ItemInterops/ConveyorEntityInterop.cs
+++ ItemInterops/ConveyorEntityInterop.cs
@@ -1,79 +1,112 @@
 ﻿using System;
 
-namespace FortressCraft.Community.ItemInterops {
-    internal class ConveyorEntityInterop : ItemInteropInterface {
-        public int GetFreeSpace(SegmentEntity entity) {
-            var conveyor = entity.As<ConveyorEntity>();
+namespace FortressCraft.Community.ItemInterops
+{
+	internal class ConveyorEntityInterop : ItemInteropInterface
+	{
+		public int GetFreeSpace(SegmentEntity entity)
+		{
+			var conveyor = entity.As<ConveyorEntity>();
 
-            if (conveyor.mbReadyToConvey) return 1;
-            return 0; 
-        }
+			return conveyor.mbReadyToConvey ? 1 : 0;
+		}
 
-        public bool GiveItem(SegmentEntity entity, ItemBase item) {
-            if (GetFreeSpace(entity) == 1) {
-                var conveyor = entity.As<ConveyorEntity>();
-                if (item.mType == ItemType.ItemCubeStack) {
-                    ItemCubeStack cube = item as ItemCubeStack;
-                    conveyor.AddCube(cube.mCubeType, cube.mCubeValue, 1);
-                    return true;
-                }
-                conveyor.AddItem(item);
-                return true;
-            }
-            return false;
-        }
+		public bool GiveItem(SegmentEntity entity, ItemBase item)
+		{
+			var conveyor = entity.As<ConveyorEntity>();
+			if (!this.IsReady(conveyor))
+				return false;
 
-        public bool HasFreeSpace(SegmentEntity entity, uint amount) {
-            if (amount > 1) return false;
-            return GetFreeSpace(entity) >= amount;
-        }
-        
-        public bool HasItem(SegmentEntity entity, ItemBase item) {
-            if (GetFreeSpace(entity) == 1) return false;
-            var conveyor = entity.As<ConveyorEntity>();
-            if(item.mType == ItemType.ItemCubeStack) {
-                ItemCubeStack cube = item as ItemCubeStack;
-                if(conveyor.mCarriedCube == cube.mCubeType && conveyor.mCarriedValue == cube.mCubeValue) return true;
-                return false;
-            }
-            return item.CompareDeep(conveyor.mCarriedItem);
-        }
+			if (item.mType == ItemType.ItemCubeStack)
+			{
+				var cube = item.As<ItemCubeStack>();
+				conveyor.AddCube(cube.mCubeType, cube.mCubeValue, 1);
+				return true;
+			}
 
-        public bool HasItems(SegmentEntity entity) {
-            return GetFreeSpace(entity) > 0;
-        }
+			conveyor.AddItem(item);
+			return true;
+		}
 
-        public bool HasItems(SegmentEntity entity, ItemBase item, out int amount) {
-            amount = 0;
-            if (GetFreeSpace(entity) == 1) return false;
-            if(HasItem(entity, item)) {
-                amount = 1;
-                return true;
-            }
-            return false;
-        }
+		public bool HasFreeSpace(SegmentEntity entity, uint amount)
+		{
+			if (amount > 1)
+				return false;
+			return GetFreeSpace(entity) >= amount;
+		}
 
-        public ItemBase TakeAnyItem(SegmentEntity entity) {
-            if (GetFreeSpace(entity) == 1) return null;
-            var conveyor = entity as ConveyorEntity;
-            ItemBase ret = null;
-            if (conveyor.mCarriedCube != 0) {
-                ret = ItemManager.SpawnCubeStack(conveyor.mCarriedCube, conveyor.mCarriedValue, 1);
-            } else {
-                ret = conveyor.mCarriedItem;
-            }
+		public bool HasItem(SegmentEntity entity, ItemBase item)
+		{
+			var conveyor = entity.As<ConveyorEntity>();
+			return this.HasItem(conveyor, item);
+		}
 
-            if (ret == null) return null;
-            conveyor.RemoveCube();
-            conveyor.RemoveItem();
-            conveyor.FinaliseOffloadingCargo();
-            return ret;
-        }
+		public bool HasItems(SegmentEntity entity)
+		{
+			return GetFreeSpace(entity) > 0;
+		}
 
-        public ItemBase TakeItem(SegmentEntity entity, ItemBase item) {
-            if (GetFreeSpace(entity) == 1) return null;
-            if (!HasItem(entity, item)) return null;
-            return TakeAnyItem(entity);
-        }
-    }
+		public bool HasItems(SegmentEntity entity, ItemBase item, out int amount)
+		{
+			amount = 0;
+			var conveyor = entity.As<ConveyorEntity>();
+			if (!this.HasItem(conveyor, item))
+				return false;
+
+			amount = 1;
+			return true;
+		}
+
+		public ItemBase TakeAnyItem(SegmentEntity entity)
+		{
+			var conveyor = entity.As<ConveyorEntity>();
+			return this.TakeAnyItem(conveyor);
+		}
+
+		public ItemBase TakeItem(SegmentEntity entity, ItemBase item)
+		{
+			var conveyor = entity.As<ConveyorEntity>();
+			if (!this.HasItem(conveyor, item))
+				return null;
+
+			return this.TakeAnyItem(conveyor);
+		}
+
+		// Private methods to avoid double casts
+
+		private Boolean IsReady(ConveyorEntity conveyor)
+		{
+			return conveyor.mbReadyToConvey;
+		}
+
+		private Boolean HasItem(ConveyorEntity conveyor, ItemBase item)
+		{
+			if (this.IsReady(conveyor))
+				return false;
+
+			if (item.mType != ItemType.ItemCubeStack)
+				return item.CompareDeep(conveyor.mCarriedItem);
+
+			ItemCubeStack cube = item.As<ItemCubeStack>();
+			return conveyor.mCarriedCube == cube.mCubeType && conveyor.mCarriedValue == cube.mCubeValue;
+		}
+
+		private ItemBase TakeAnyItem(ConveyorEntity conveyor)
+		{
+			if (this.IsReady(conveyor))
+				return null;
+
+			ItemBase returnItem = conveyor.mCarriedCube != 0
+				? ItemManager.SpawnCubeStack(conveyor.mCarriedCube, conveyor.mCarriedValue, 1)
+				: conveyor.mCarriedItem;
+
+			if (returnItem == null)
+				return null;
+
+			conveyor.RemoveCube();
+			conveyor.RemoveItem();
+			conveyor.FinaliseOffloadingCargo();
+			return returnItem;
+		}
+	}
 }
